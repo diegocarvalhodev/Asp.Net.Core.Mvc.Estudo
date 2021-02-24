f0566982fd79bcede02b9394847db7e422dc9e77
Diego Carvalho <diegomcarvalho7@gmail.com>
2021-02-23 21:57:00 -0300

    Preparando o Objeto de Resposta para a chamada AJAX.

--- Aulas/Aula1/CasaDoCodigo/wwwroot/js/carrinho.js
+++ Aulas/Aula1/CasaDoCodigo/wwwroot/js/carrinho.js
@@ -35,6 +35,10 @@ class Carrinho {
             type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify(data)
+        }).done(function (response) {
+            //location.reload();
+
+            debugger;
         });
     }
 }
