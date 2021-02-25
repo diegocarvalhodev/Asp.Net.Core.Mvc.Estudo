be29d65a29faa819a4b7e9d13161df72c6af3b58
Diego Carvalho <diegomcarvalho7@gmail.com>
2021-02-25 19:49:57 -0300

    Injetando o Cadastro na View e Redirecionando se Necessário.

--- Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -25,7 +25,7 @@ namespace CasaDoCodigo.Controllers
         }
 
         public IActionResult Carrossel()
-        {   
+        {
             return View(produtoRepository.GetProdutos());
         }
 
@@ -43,7 +43,14 @@ namespace CasaDoCodigo.Controllers
 
         public IActionResult Cadastro()
         {
-            return View();
+            var pedido = pedidoRepository.GetPedido();
+
+            if ( (pedido == null) || (pedido.Itens.Count == 0))
+            {
+                return RedirectToAction("Carrossel");
+            }
+
+            return View(pedido.Cadastro);
         }
 
         public IActionResult Resumo()
@@ -53,9 +60,9 @@ namespace CasaDoCodigo.Controllers
         }
 
         [HttpPost] //Atributo para permitir apenas requisições do tipo POST
-        public UpdateQuantidadeResponse UpdateQuantidade([FromBody]ItemPedido itemPedido)
-            /*O atributo [FromBody] sinalizar que o valor, enviado
-              na requisição, faz parte do corpo da requisição.*/
+        public UpdateQuantidadeResponse UpdateQuantidade([FromBody] ItemPedido itemPedido)
+        /*O atributo [FromBody] sinalizar que o valor, enviado
+          na requisição, faz parte do corpo da requisição.*/
         {
             return pedidoRepository.UpdateQuantidade(itemPedido);
         }
